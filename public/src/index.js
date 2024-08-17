f36a25d9e3938994339537ab7d1df6f6a818b659
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-17 16:50:31 +0530

    componets

--- public/src/index.js
+++ public/src/index.js
@@ -3,12 +3,22 @@ import {createroot} from "react-dom/client";
 const root = createroot(document.getElementById("root"));
 
 
-//jsx has two rrule all tages must be close and there is one parent elemnt
+//jsx has two rule all tages must be close and there is one parent elemnt
 function Sayhello()
 {
     console.log("hello Rupesh") ;
+}
+// rule for  componets
+//its as simlar as javascript funtion
+//componets name should be in pascal case uppercameel case
+//and function must be return jsx 
+function Nilesh()
+
+{
+
 }
 root.render(<><h1 >First react app {2+2}</h1>
     <h1>HII guys</h1></>
 )// jsx {is jsx exprssion} //
 //<></> is called jsx fragment
+
