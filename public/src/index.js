a182ade8954a98068902165585c5df0e776e763e
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-17 16:44:47 +0530

    Two rule of jsx

--- public/src/index.js
+++ public/src/index.js
@@ -3,9 +3,9 @@ import {createroot} from "react-dom/client";
 const root = createroot(document.getElementById("root"));
 
 
-
+//jsx has two rrule all tages must be close and there is one parent elemnt
 function Sayhello()
 {
     console.log("hello Rupesh") ;
 }
-root.render(<h1 >First react app {2+2}</h1>)// jsx {is jsx exprssion}
+root.render(<h1 >First react app {2+2}</h1>)// jsx {is jsx exprssion} 
